60502ea65bd500d17b215d7f4eb58a97da37c29b
Lana <matic02lana@gmail.com>
2025-09-10 16:08:33 +0200

    Project init: CMakeLists.txt, Lishe.h, Lishe.cpp

---
+++ Lishe.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "llvm/IR/PassManager.h"
+
+namespace llvm {
+
+/// Loop-Invariant Store Hoisting/Elimination (LISHE)
+struct LISHEPass : public PassInfoMixin<LISHEPass> {
+  PreservedAnalyses run(Function &F, FunctionAnalysisManager &FAM);
+};
+
+}
